d20a212e222e7bf89f63a7141d10e654d4ced124
Alien-Love <vitor_f_vidal@hotmail.com>
2022-10-27 00:16:26 -0300

    adiciona opcao 'finalizar'

--- controladorprincipal.py
+++ controladorprincipal.py
@@ -1,3 +1,4 @@
+import sys
 from telainicial import TelaInicial
 from controladoreleitor import ControladorEleitor
 from controladorvoto import ControladorVoto
@@ -11,17 +12,21 @@ class ControladorPrincipal:
     def __init__(self):
         self.__tela_inicial = TelaInicial()
         self.__controlador_eleitor = ControladorEleitor(self)
-        self.__controlador_voto = ControladorVoto(self) #tentar fazer com getter do controlador principal
+        self.__controlador_voto = ControladorVoto(self)
         #self.__controlador_urna = ControladorUrna(self)
         self.__controlador_candidato = ControladorCandidato(self)
         self.__resultado = Resultado(self)
 
     def inicia(self):    #inicia opçao de voto, cadastro, ou resultado
-        opcoes = {1: self.inicia_voto, 2: self.inicia_cadastro, 0: self.inicia_resultado}
+        opcoes = {0: self.finaliza_sistema, 1: self.inicia_voto, 2: self.inicia_cadastro, 3: self.inicia_resultado}
 
         while True:
             opcao = (self.__tela_inicial.tela_opcoes())
             opcoes[opcao]()
+
+    @property
+    def controlador_eleitor(self):
+        return self.__controlador_eleitor
 
     def inicia_voto(self):
         self.__controlador_eleitor.inclui_eleitor()
@@ -33,3 +38,6 @@ class ControladorPrincipal:
 
     def inicia_resultado(self):
         self.__resultado.calcular_resultado()
+    
+    def finaliza_sistema(self):
+        sys(exit)
--- telainicial.py
+++ telainicial.py
@@ -1,5 +1,12 @@
 class TelaInicial:
 
     def tela_opcoes(self):
+        print("*"*20)
+        print("Urna Eletronica")
+        print("1 - Iniciar Voto")
+        print("2 - Cadastro de Candidato")
+        print("3 - Resultados")
+        print("0 - Sair")
         opcao = input("digite a opcao:")
+        print("*"*20)
         return int(opcao)
