b3b6181b30a4d78f13b1fc60c375d54e0963a8f3
Alien-Love <vitor_f_vidal@hotmail.com>
2022-10-29 22:36:16 -0300

    criacao de classe abstrata para telas

--- telaeleitor.py
+++ telaeleitor.py
@@ -1,7 +1,14 @@
-class TelaEleitor:
+from abstracttela import AbstractTela
+
+class TelaEleitor(AbstractTela):
+    def __init__(self):
+        super().__init__()
     #nome deve ser uma string de 3 ou mais caracteres
     #cpf deve ser um inteiro de 11 digitos
     #tipo sera um inteiro de 1 a 3: {1: 'aluno', 2: 'professor', 3: 'tecnico administrativo'}
+    def le_inteiro(self):
+        return super().le_inteiro()
+
     def cadastro_eleitor(self):
        a = input("nome do eleitor:")
        b = input("cpf:")
